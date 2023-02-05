c64ae688654a6c06fb8850aecf9888a9d3ae95bc
frc195programming <frc195programming@gmail.com>
2023-02-05 02:24:42 +0000

    updated virtual arm and added faults

--- src/arm_node/main.py
+++ src/arm_node/main.py
@@ -15,12 +15,34 @@ import math
 
 def publish_arm_base_link(degrees : float):
     transform = Transform()
-    transform.linear.z = .25 # Quarter of a meter up? - need to check cad for pivot point of base arm
+    transform.linear.z = 0.24511 # Quarter of a meter up? - need to check cad for pivot point of base arm
     transform.angular.pitch = math.radians(degrees)
 
     transform_link = TransformLink("arm_base", "base_link") # originate from base link which is the center of the robot
     transform_link.set_transform(transform)
     transform_link.publish()
+
+def publish_arm_upper_link(degrees : float):
+    transform = Transform()
+    transform.linear.z = 0.847725 # 0.87630
+
+    transform.angular.pitch = math.radians(degrees)
+
+    transform_link = TransformLink("arm_upper", "arm_base")
+    transform_link.set_transform(transform)
+    transform_link.publish()
+
+def publish_arm_extender_link(degrees : float, extension : float):
+    transform = Transform()
+   # transform.linear.z = 0 # 
+    transform.angular.pitch = math.radians(degrees)
+    transform.linear.z = extension * 0.3556 + 0.2032
+
+    transform_link = TransformLink("arm_extender", "arm_upper")
+    transform_link.set_transform(transform)
+    transform_link.publish()
+
+
 
 def ros_func():
     global robot_status
@@ -44,6 +66,7 @@ def ros_func():
             if robot_status.get_mode() == RobotMode.TELEOP:
                 armBaseMaster.set(ControlMode.MOTION_MAGIC, control_sub.get().arm_base_requested_position, 0.0)
                 secondArmMaster.set(ControlMode.MOTION_MAGIC, control_sub.get().arm_upper_requested_position, 0.0)
+                
                 pass
             else:
                 armBaseMaster.set(ControlMode.PERCENT_OUTPUT, 0.0, 0.0)
@@ -55,7 +78,13 @@ def ros_func():
             else:
                 extension_solenoid.set(SolenoidState.OFF)
 
-        publish_arm_base_link(45)#armBaseMaster.get_sensor_position() * 360.0)  #MGT pretend this is 45 for now for demo purposes
+        publish_arm_base_link(armBaseMaster.get_sensor_position() * 360.0)#armBaseMaster.get_sensor_position() * 360.0)  #MGT pretend this is 45 for now for demo purposes
+        publish_arm_upper_link(90)#secondArmMaster.get_sensor_position() * 360.0)
+        publish_arm_extender_link(secondArmMaster.get_sensor_position() * 360.0, True)#extension_solenoid.get() == SolenoidState.ON)
+       
+        
+        master_sticky_faults = armBaseMaster.get_sticky_faults()
+        slave_sticky_faults = armBaseSlave.get_sticky_faults()
 
         status_message = Arm_Status()
         secondArmMaster.get_sensor_position()
@@ -63,7 +92,17 @@ def ros_func():
         armBaseMaster.get_sensor_position()
         status_message.arm_upper_actual_position = 0
         status_message.extended = extension_solenoid.get() == SolenoidState.ON
+        status_message.left_arm_base_remote_loss_of_signal = master_sticky_faults.RemoteLossOfSignal
+        status_message.right_arm_base_remote_loss_of_signal = slave_sticky_faults.RemoteLossOfSignal
+        status_message.left_arm_base_reset_during_en = master_sticky_faults.ResetDuringEn
+        status_message.right_arm_base_reset_during_en = slave_sticky_faults.ResetDuringEn
+        status_message.left_arm_base_hardware_ESD_reset = master_sticky_faults.HardwareESDReset
+        status_message.right_arm_base_hardware_ESD_reset = slave_sticky_faults.HardwareESDReset
+        status_message.left_arm_base_supply_unstable = master_sticky_faults.SupplyUnstable
+        status_message.right_arm_base_supply_unstable = slave_sticky_faults.SupplyUnstable
         status_publisher.publish(status_message)
+        
+        
 
         rate.sleep()
 
@@ -82,11 +121,29 @@ def ros_main(node_name):
 
     arm_cube = Cube("robot_parts", 1, "arm_base")
     arm_transform = Transform()
-    arm_transform.linear.z = 0.5
+    arm_transform.linear.z = 0.56515
     arm_cube.set_transform(arm_transform)
-    arm_cube.set_scale(Scale(0.1, 0.5, 1.0))
+    arm_cube.set_scale(Scale(0.0508, 0.508, 1.1303)) #44.5 tall, 20 apart
     arm_cube.set_color(Color(.7, .7, .7, 1.0))
     arm_cube.publish()
+
+    arm_upper_cube = Cube("robot_parts", 2, "arm_upper")
+    arm_upper_transform = Transform()
+    arm_upper_transform.linear.z = 0.3048
+    arm_upper_cube.set_transform(arm_upper_transform)
+    arm_upper_cube.set_scale(Scale(0.0508, 0.381, 0.6096)) #24, 15, 2
+    arm_upper_cube.set_color(Color(.7, .7, .7, 1.0))
+    arm_upper_cube.publish()
+
+    arm_extender_cube = Cube("robot_parts", 3, "arm_extender")
+    arm_extender_transform = Transform()
+    arm_extender_transform.linear.z = 0.2032 #12 in
+    arm_extender_cube.set_transform(arm_extender_transform)
+    arm_extender_cube.set_scale(Scale(0.0508, 0.304673, 0.4064)) #14 out (11.995, 16, 2) 
+    arm_extender_cube.set_color(Color(.7, .7, .7, 1.0))
+    arm_extender_cube.publish()
+
+  
 
     t1 = Thread(target=ros_func)
     t1.start()
