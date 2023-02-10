e3bd07d1a9203077175533ddd9c8e060a01cecf5
Robert Hilton <robert.a.hilton.jr@gmail.com>
2023-02-10 06:02:26 +0000

    Fix arm simulation orientation

--- src/arm_node/arm_simulation.py
+++ src/arm_node/arm_simulation.py
@@ -1,9 +1,11 @@
 from ck_utilities_py_node.transform_links import *
 from ck_utilities_py_node.rviz_shapes import *
 import math
+from ck_utilities_py_node.ckmath import *
 
 class ArmSimulation:
     def __init__(self) -> None:
+        #Bumpers
         drivebase = Cube("base_link")
         drivebase.set_scale(Scale(0.6096, 0.6096, 0.127))
         drivetrans = Transform()
@@ -12,35 +14,88 @@ class ArmSimulation:
         drivebase.set_color(Color(.5, 0, 1.0, 1.0))
         drivebase.publish()
 
-        arm_cube = Cube("arm_base")
-        arm_transform = Transform()
-        arm_transform.linear.z = 0.56515
-        arm_cube.set_transform(arm_transform)
-        arm_cube.set_scale(Scale(0.0508, 0.508, 1.1303)) #44.5 tall, 20 apart
-        arm_cube.set_color(Color(.7, .7, .7, 1.0))
-        arm_cube.publish()
 
-        arm_upper_cube = Cube("arm_upper")
-        arm_upper_transform = Transform()
-        arm_upper_transform.linear.z = 0.3048
-        arm_upper_cube.set_transform(arm_upper_transform)
-        arm_upper_cube.set_scale(Scale(0.0508, 0.381, 0.6096)) #24, 15, 2
-        arm_upper_cube.set_color(Color(.7, .7, .7, 1.0))
-        arm_upper_cube.publish()
+        base_arm_1 = Cube("arm_base")
+        base_arm_1_transform = Transform()
+        base_arm_1_transform.linear.y = inches_to_meters(9.5)
+        base_arm_1_transform.linear.z = inches_to_meters(18.25)
+        base_arm_1.set_transform(base_arm_1_transform)
+        base_arm_1.set_scale(Scale(inches_to_meters(2), inches_to_meters(1), inches_to_meters(44.5))) #44.5 tall, 20 apart
+        base_arm_1.set_color(Color(.7, .7, .7, 1.0))
+        base_arm_1.publish()
 
-        arm_extender_cube = Cube("arm_extender")
-        arm_extender_transform = Transform()
-        arm_extender_transform.linear.z = 0.2032 #12 in
-        arm_extender_cube.set_transform(arm_extender_transform)
-        arm_extender_cube.set_scale(Scale(0.0508, 0.304673, 0.4064)) #14 out (11.995, 16, 2)
-        arm_extender_cube.set_color(Color(.7, .7, .7, 1.0))
-        arm_extender_cube.publish()
+        base_arm_2 = Cube("arm_base")
+        base_arm_2_transform = Transform()
+        base_arm_2_transform.linear.y = inches_to_meters(-9.5)
+        base_arm_2_transform.linear.z = inches_to_meters(18.25)
+        base_arm_2.set_transform(base_arm_2_transform)
+        base_arm_2.set_scale(Scale(inches_to_meters(2), inches_to_meters(1), inches_to_meters(44.5))) #44.5 tall, 20 apart
+        base_arm_2.set_color(Color(.7, .7, .7, 1.0))
+        base_arm_2.publish()
+
+        base_arm_cross = Cube("arm_base")
+        base_arm_cross_transform = Transform()
+        base_arm_cross_transform.linear.z = inches_to_meters(40)
+        base_arm_cross.set_transform(base_arm_cross_transform)
+        base_arm_cross.set_scale(Scale(inches_to_meters(2), inches_to_meters(18), inches_to_meters(1))) #44.5 tall, 20 apart
+        base_arm_cross.set_color(Color(.7, .7, .7, 1.0))
+        base_arm_cross.publish()
+
+        upper_arm_1 = Cube("arm_upper")
+        upper_arm_1_transform = Transform()
+        upper_arm_1_transform.linear.y = inches_to_meters(7)
+        upper_arm_1_transform.linear.z = inches_to_meters(-8)
+        upper_arm_1.set_transform(upper_arm_1_transform)
+        upper_arm_1.set_scale(Scale(inches_to_meters(2), inches_to_meters(1), inches_to_meters(24))) #24, 15, 2
+        upper_arm_1.set_color(Color(.7, .7, .7, 1.0))
+        upper_arm_1.publish()
+
+        upper_arm_2 = Cube("arm_upper")
+        upper_arm_2_transform = Transform()
+        upper_arm_2_transform.linear.y = inches_to_meters(-7)
+        upper_arm_2_transform.linear.z = inches_to_meters(-8)
+        upper_arm_2.set_transform(upper_arm_2_transform)
+        upper_arm_2.set_scale(Scale(inches_to_meters(2), inches_to_meters(1), inches_to_meters(24))) #24, 15, 2
+        upper_arm_2.set_color(Color(.7, .7, .7, 1.0))
+        upper_arm_2.publish()
+
+        upper_arm_cross = Cube("arm_upper")
+        upper_arm_cross_transform = Transform()
+        upper_arm_cross_transform.linear.z = inches_to_meters(3.5)
+        upper_arm_cross.set_transform(upper_arm_cross_transform)
+        upper_arm_cross.set_scale(Scale(inches_to_meters(2), inches_to_meters(13), inches_to_meters(1)))
+        upper_arm_cross.set_color(Color(.7, .7, .7, 1.0))
+        upper_arm_cross.publish()
+
+        arm_extender_1_cube = Cube("arm_extender")
+        arm_extender_1_transform = Transform()
+        arm_extender_1_transform.linear.y = inches_to_meters(-5.420734)
+        arm_extender_1_cube.set_transform(arm_extender_1_transform)
+        arm_extender_1_cube.set_scale(Scale(inches_to_meters(2), inches_to_meters(1), inches_to_meters(18)))
+        arm_extender_1_cube.set_color(Color(.7, .7, .7, 1.0))
+        arm_extender_1_cube.publish()
+
+        arm_extender_2_cube = Cube("arm_extender")
+        arm_extender_2_transform = Transform()
+        arm_extender_2_transform.linear.y = inches_to_meters(5.420734)
+        arm_extender_2_cube.set_transform(arm_extender_2_transform)
+        arm_extender_2_cube.set_scale(Scale(inches_to_meters(2), inches_to_meters(1), inches_to_meters(18)))
+        arm_extender_2_cube.set_color(Color(.7, .7, .7, 1.0))
+        arm_extender_2_cube.publish()
+
+        arm_extender_cross_cube = Cube("arm_extender")
+        arm_extender_cross_transform = Transform()
+        arm_extender_cross_transform.linear.z = inches_to_meters(-8.5)
+        arm_extender_cross_cube.set_transform(arm_extender_cross_transform)
+        arm_extender_cross_cube.set_scale(Scale(inches_to_meters(2), inches_to_meters(10), inches_to_meters(1)))
+        arm_extender_cross_cube.set_color(Color(.7, .7, .7, 1.0))
+        arm_extender_cross_cube.publish()
 
         wrist_cube = Cube("wrist_link")
         wrist_transform = Transform()
         wrist_transform.linear.z = 0.005
         wrist_cube.set_transform(wrist_transform)
-        wrist_cube.set_scale(Scale(0.1, 0.1, 0.01))
+        wrist_cube.set_scale(Scale(inches_to_meters(5), inches_to_meters(5), inches_to_meters(1.5)))
         wrist_cube.set_color(Color(1, .7, .7, 1.0))
         wrist_cube.publish()
 
@@ -48,7 +103,7 @@ class ArmSimulation:
 
     def publish_arm_base_link(self, degrees : float):
         transform = Transform()
-        transform.linear.z = 0.24511 # Quarter of a meter up? - need to check cad for pivot point of base arm
+        transform.linear.z = inches_to_meters(7.75222441) #Arm rotation point is 7.75222441 in off the ground
         transform.angular.pitch = math.radians(degrees)
 
         transform_link = TransformLink("arm_base", "base_link") # originate from base link which is the center of the robot
@@ -58,7 +113,7 @@ class ArmSimulation:
 
     def publish_arm_upper_link(self, degrees : float):
         transform = Transform()
-        transform.linear.z = 0.847725 # 0.87630
+        transform.linear.z = inches_to_meters(34.50000000) #Upper joint rotation is 34.5 in above base joint
 
         transform.angular.pitch = math.radians(degrees)
 
@@ -69,7 +124,7 @@ class ArmSimulation:
 
     def publish_arm_extender_link(self, extension : float):
         transform = Transform()
-        transform.linear.z = extension * 0.3556 + 0.2032
+        transform.linear.z = extension * inches_to_meters(-12) - inches_to_meters(12.436533)
 
         transform_link = TransformLink("arm_extender", "arm_upper")
         transform_link.set_transform(transform)
@@ -79,7 +134,7 @@ class ArmSimulation:
     def publish_arm_wrist_link(self, degrees : float):
         transform = Transform()
         transform.angular.yaw = math.radians(degrees)
-        transform.linear.z = 0.3964
+        transform.linear.z = inches_to_meters(-9.763878)
         transform_link = TransformLink("wrist_link", "arm_extender")
         transform_link.set_transform(transform)
         transform_link.publish()
