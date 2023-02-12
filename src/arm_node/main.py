06e680b0403bc0701350b9c05ea161cb685d6d9c
BonomiC <bonomi.c11@gmail.com>
2023-02-12 23:07:52 +0000

    Simple arm control

--- src/arm_node/main.py
+++ src/arm_node/main.py
@@ -8,7 +8,7 @@ from ck_utilities_py_node.rviz_shapes import *
 from ck_utilities_py_node.solenoid import *
 from frc_robot_utilities_py_node.frc_robot_utilities_py import *
 from frc_robot_utilities_py_node.RobotStatusHelperPy import RobotStatusHelperPy, Alliance, RobotMode, BufferedROSMsgHandlerPy
-from ck_ros_msgs_node.msg import Arm_Control, Arm_Status, Fault, Health_Monitor_Control
+from ck_ros_msgs_node.msg import Arm_Control, Arm_Goal, Arm_Status, Fault, Health_Monitor_Control
 from arm_node.arm_simulation import ArmSimulation
 from ck_utilities_py_node.constraints import *
 from arm_node.arm_constraints import ArmConstraints
@@ -22,27 +22,73 @@ def ros_func():
 
     control_sub = BufferedROSMsgHandlerPy(Arm_Control)
     control_sub.register_for_updates("ArmControl")
+    goal_sub = BufferedROSMsgHandlerPy(Arm_Goal)
+    goal_sub.register_for_updates("ArmGoal")
     status_publisher = rospy.Publisher(name="ArmStatus", data_class=Arm_Status, queue_size=50, tcp_nodelay=True)
 
     baseArmMaster = Motor("baseArmMaster", MotorType.TalonFX)
     baseArmFollower = Motor("baseArmFollower", MotorType.TalonFX)
     upperArmMaster = Motor("upperArmMaster", MotorType.TalonFX)
-    upperBaseFollower = Motor("upperArmFollower", MotorType.TalonFX)
+    upperArmFollower = Motor("upperArmFollower", MotorType.TalonFX)
 
     wristMotor = Motor("wristMotor", MotorType.TalonFX)
 
-    extension_solenoid = Solenoid("extension", SolenoidType.SINGLE)
+    base_brake_solenoid = Solenoid("base_brake", SolenoidType.SINGLE)
+    upper_brake_solenoid = Solenoid("upper_brake", SolenoidType.SINGLE)
+
+    # extension_solenoid = Solenoid("extension", SolenoidType.SINGLE)
 
     rate = rospy.Rate(20)
 
-    state_machine = ArmStateMachine(baseArmMaster, upperArmMaster, wristMotor, extension_solenoid)
+    state_machine = ArmStateMachine(baseArmMaster, upperArmMaster, wristMotor, base_brake_solenoid, upper_brake_solenoid, None)
 
     while not rospy.is_shutdown():
         robot_mode = robot_status.get_mode()
+        goal: Arm_Goal = goal_sub.get()
+
+        real_goal = None
+
+        if goal is not None:
+            if goal.goal == Arm_Goal.HOME:
+                real_goal = ArmStateMachine.States.HOME
+            elif goal.goal == Arm_Goal.SHELF_PICKUP_FRONT:
+                real_goal = ArmStateMachine.States.SHELF_FRONT
+            elif goal.goal == Arm_Goal.HIGH_CUBE_FRONT:
+                real_goal = ArmStateMachine.States.HIGH_CUBE_FRONT
+            
+            # print('Setting goal to:', real_goal)
+        else:
+            real_goal = state_machine.goal_state
+
         
         if robot_mode == RobotMode.TELEOP:
+            print('is teleop')
+            # baseArmMaster.set_neutral_mode(NeutralMode.Coast)
+            # baseArmFollower.set_neutral_mode(NeutralMode.Coast)
+            # upperArmMaster.set_neutral_mode(NeutralMode.Coast)
+            # upperArmFollower.set_neutral_mode(NeutralMode.Coast)
+            # baseArmMaster.apply()
+            # upperArmMaster.apply()
+            base_brake_solenoid.set(SolenoidState.ON)
+            upper_brake_solenoid.set(SolenoidState.ON)
+            # print("set on")
+            # baseArmMaster.set(ControlMode.PERCENT_OUTPUT, 0.0)
+            # upperArmMaster.set(ControlMode.PERCENT_OUTPUT, 0.0)
+            # wristMotor.set(ControlMode.PERCENT_OUTPUT, 0.0)        
+
+            state_machine.goal_state = real_goal
             state_machine.step()
-        elif robot_status.get_mode() == RobotMode.DISABLED:
+
+        elif robot_mode == RobotMode.DISABLED:
+            # baseArmMaster.set_neutral_mode(NeutralMode.Brake)
+            # baseArmFollower.set_neutral_mode(NeutralMode.Brake)
+            # upperArmMaster.set_neutral_mode(NeutralMode.Brake)
+            # upperArmFollower.set_neutral_mode(NeutralMode.Brake)
+            # baseArmMaster.apply()
+            # upperArmMaster.apply()
+            base_brake_solenoid.set(SolenoidState.OFF)
+            upper_brake_solenoid.set(SolenoidState.OFF)
+            # print("set off")
             baseArmMaster.set(ControlMode.PERCENT_OUTPUT, 0.0)
             upperArmMaster.set(ControlMode.PERCENT_OUTPUT, 0.0)
             wristMotor.set(ControlMode.PERCENT_OUTPUT, 0.0)        
@@ -86,7 +132,7 @@ def ros_func():
         status_message.arm_base_actual_position = baseArmMaster.get_sensor_position()
         status_message.arm_upper_actual_position = upperArmMaster.get_sensor_position()
         status_message.arm_wrist_actual_position = wristMotor.get_sensor_position()
-        status_message.extended = extension_solenoid.get() == SolenoidState.ON
+        # status_message.extended = extension_solenoid.get() == SolenoidState.ON
         status_message.left_arm_base_remote_loss_of_signal = master_sticky_faults.RemoteLossOfSignal
         status_message.right_arm_base_remote_loss_of_signal = follower_sticky_faults.RemoteLossOfSignal
         status_message.left_arm_base_reset_during_en = master_sticky_faults.ResetDuringEn
--- src/arm_node/state_machine.py
+++ src/arm_node/state_machine.py
@@ -1,3 +1,5 @@
+from arm_node.positions import *
+
 from ck_utilities_py_node.motor import *
 from ck_utilities_py_node.solenoid import *
 from ck_utilities_py_node.StateMachine import StateMachine
@@ -11,31 +13,41 @@ class ArmStateMachine(StateMachine):
 
 
     class States(Enum):
-        HOME=1
-        INTERMEDIATE_FRONT=2
-        PENDULUM=3
+        HOME=1,
+        INTERMEDIATE_FRONT=2,
+        GROUND_CUBE_FRONT=3,
+        GROUND_CONE_FRONT=4,
+        GROUND_DEAD_CODE_FRONT=5,
+        SHELF_FRONT=6,
+        LOW_SCORE_FRONT=7,
+        MID_CUBE_FRONT=8,
+        HIGH_CUBE_FRONT=9,
+        MID_CONE_FRONT=10,
+        HIGH_CONE_FRONT=11,
+        PENDULUM=12
+
 
     class HomeState(StateMachine.State):
 
         def __init__(self, machine):
             self.machine: ArmStateMachine = machine
-            self.__entered_time = None
 
         def get_enum(self):
             return ArmStateMachine.States.HOME
 
         def entry(self):
-            self.__entered_time = rospy.Time().now().to_sec()
+            # print('Entering', self.get_enum())
+            pass
 
         def step(self):
-            self.machine.baseMotor.set(ControlMode.PERCENT_OUTPUT, 0)
-            self.machine.upperMotor.set(ControlMode.PERCENT_OUTPUT, 0)
+            self.machine.baseMotor.set(ControlMode.MOTION_MAGIC, POS_HOME.base_position)
+            self.machine.upperMotor.set(ControlMode.MOTION_MAGIC, POS_HOME.upper_position)
 
         def transition(self) -> str:
-            if robot_status.get_mode() == RobotMode.TELEOP:
-                return ArmStateMachine.States.PENDULUM
+            if self.machine.goal_state is not self.get_enum():
+                return ArmStateMachine.States.INTERMEDIATE_FRONT
 
-            return ArmStateMachine.States.HOME
+            return self.get_enum()
 
     class IntermediateFrontState(StateMachine.State):
 
@@ -47,19 +59,68 @@ class ArmStateMachine(StateMachine):
             return ArmStateMachine.States.INTERMEDIATE_FRONT
 
         def entry(self):
-
-            self.__entered_time = rospy.Time().now().to_sec()
-
-        def step(self):
-            print(self.get_enum())
+            # print('Entering', self.get_enum())
+            # self.machine.extensionSolenoid.set(SolenoidState.OFF)
             pass
 
+        def step(self):
+            self.machine.baseMotor.set(ControlMode.MOTION_MAGIC, POS_INTERMEDIATE.base_position)
+            self.machine.upperMotor.set(ControlMode.MOTION_MAGIC, POS_INTERMEDIATE.upper_position)
+
         def transition(self) -> str:
-            if rospy.Time().now().to_sec() - self.__entered_time > 1.0:
-                return ArmStateMachine.States.HOME
-            return ArmStateMachine.States.INTERMEDIATE_FRONT
+            delta = 0.05
+            if self.machine.baseMotor.is_at_setpoint(delta) and self.machine.upperMotor.is_at_setpoint(delta):
+                return self.machine.goal_state
+
+            return self.get_enum()
+
+    class ShelfFrontState(StateMachine.State):
+
+        def __init__(self, machine):
+            self.machine: ArmStateMachine = machine
+
+        def get_enum(self):
+            return ArmStateMachine.States.SHELF_FRONT
+
+        def entry(self):
+            # print('Entering', self.get_enum())
+            pass
+
+        def step(self):
+            self.machine.baseMotor.set(ControlMode.MOTION_MAGIC, POS_SHELF.base_position)
+            self.machine.upperMotor.set(ControlMode.MOTION_MAGIC, POS_SHELF.upper_position)
+
+        def transition(self) -> str:
+            if self.machine.goal_state is not self.get_enum():
+                return ArmStateMachine.States.INTERMEDIATE_FRONT
+
+            return self.get_enum()
+
+    class HighCubeFrontState(StateMachine.State):
+
+        def __init__(self, machine):
+            self.machine: ArmStateMachine = machine
+
+        def get_enum(self):
+            return ArmStateMachine.States.HIGH_CUBE_FRONT
+
+        def entry(self):
+            print('Entering', self.get_enum())
+            # self.machine.extensionSolenoid.set(SolenoidState.ON)
+            pass
+
+        def step(self):
+            self.machine.baseMotor.set(ControlMode.MOTION_MAGIC, POS_HIGH_CUBE.base_position)
+            self.machine.upperMotor.set(ControlMode.MOTION_MAGIC, POS_HIGH_CUBE.upper_position)
+
+        def transition(self) -> str:
+            if self.machine.goal_state is not self.get_enum():
+                return ArmStateMachine.States.INTERMEDIATE_FRONT
+
+            return self.get_enum()
 
     class PendulumState(StateMachine.State):
+
         def __init__(self, machine):
             self.machine: ArmStateMachine = machine
             self.__entered_time = None
@@ -84,7 +145,8 @@ class ArmStateMachine(StateMachine):
             upper_home = 0.5
             upper_output = upper_range * math.sin(speed * time) - upper_home
 
-            self.machine.upperMotor.set(ControlMode.POSITION, output)
+            self.machine.baseMotor.set(ControlMode.MOTION_MAGIC, base_output)
+            self.machine.upperMotor.set(ControlMode.MOTION_MAGIC, upper_output)
 
         def transition(self) -> str:
             if robot_status.get_mode() == RobotMode.DISABLED:
@@ -92,15 +154,19 @@ class ArmStateMachine(StateMachine):
 
             return ArmStateMachine.States.PENDULUM
 
-    def __init__(self, baseMotor: Motor, upperMotor: Motor, wristMotor: Motor, extensionSolenoid: Solenoid):
+    def __init__(self, baseMotor: Motor, upperMotor: Motor, wristMotor: Motor, baseBrakeSolenoid: Solenoid, upperBrakeSolenoid: Solenoid, extensionSolenoid: Solenoid):
         self.baseMotor = baseMotor
         self.upperMotor = upperMotor
         self.wristMotor = wristMotor
+        self.baseBrakeSolenoid = baseBrakeSolenoid
+        self.upperBrakeSolenoid = upperBrakeSolenoid
         self.extensionSolenoid = extensionSolenoid
 
         states = {
             ArmStateMachine.States.HOME : ArmStateMachine.HomeState(self),
             ArmStateMachine.States.INTERMEDIATE_FRONT : ArmStateMachine.IntermediateFrontState(self),
+            ArmStateMachine.States.SHELF_FRONT : ArmStateMachine.ShelfFrontState(self),
+            ArmStateMachine.States.HIGH_CUBE_FRONT : ArmStateMachine.HighCubeFrontState(self),
             ArmStateMachine.States.PENDULUM : ArmStateMachine.PendulumState(self),
         }
 
