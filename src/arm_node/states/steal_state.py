068d1848d03e37023ad1c027a89cc9362355d4b9
frc195programming <frc195programming@gmail.com>
2023-02-15 01:49:28 +0000

    New states for review.

---
+++ src/arm_node/states/steal_state.py
@@ -0,0 +1,41 @@
+from arm_node.arm import Arm
+from arm_node.positions import *
+from arm_node.states.util import *
+from arm_node.state_machine import ArmStateMachine
+
+from ck_utilities_py_node.motor import *
+from ck_utilities_py_node.solenoid import *
+from ck_utilities_py_node.StateMachine import StateMachine
+
+
+class StealState(StateMachine.State):
+
+    def __init__(self, machine, arm, is_front=True):
+        self.machine: ArmStateMachine = machine
+        self.arm: Arm = arm
+        self.is_front = is_front
+
+        self.position: ArmPosition = POS_STEAL
+
+        if not is_front:
+            self.position = mirror_position(self.position)
+
+    def get_enum(self):
+        if self.is_front:
+            return ArmStateMachine.States.STEAL_FRONT
+        else:
+            return ArmStateMachine.States.STEAL_BACK
+
+    def entry(self):
+        print('Entering', self.get_enum())
+        self.arm.disable_brakes()
+        self.arm.extend()
+            
+    def step(self):
+        standard_step(self.arm, self.position)
+
+    def transition(self) -> Enum:
+        if self.machine.goal_state is not self.get_enum():
+            return transition_to_intermediate(self.is_front)
+
+        return self.get_enum()
