618bffa4a50e16222576744a876295b74a862c36
Mac #1 <frc195programming@gmail.com>
2023-04-06 18:07:36 +0000

    Updated shelf pos

--- src/arm_node/positions.py
+++ src/arm_node/positions.py
@@ -31,8 +31,8 @@ POS_GROUND_DEAD_CONE = ArmPosition(40.5, 32.5, -42, -33)
 POS_SIDEWAYS_DEAD_CONE = ArmPosition(6.5, 32.0, -6.5, -32.0)
 
 
-POS_SHELF_CUBE = ArmPosition(-14.98, 77.9, 17.98, -74.1)
-POS_SHELF_CONE = ArmPosition(-14.98, 77.9, 17.98, -74.1)
+POS_SHELF_CUBE = ArmPosition(-14.98, 78.4, 17.98, -75.1)
+POS_SHELF_CONE = ArmPosition(-14.98, 78.4, 17.98, -75.1)
 
 POS_LOW_SCORE = ArmPosition(21.38, 40.68, -21.38, -40.68)
 POS_MID_CUBE = ArmPosition(3, 70.88, 0, -70.88)
